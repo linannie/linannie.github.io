c1348adfd4826f6b0a2db31635e45471576e71cf
Annie  Lin <a.popcorn2@gmail.com>
2019-01-01 23:11:30 -0800

    use old site again

---
+++ js/about.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	//$("*").hide();
+	$("*").fadeIn(1000);
+	$("#dog1").hide();
+	$("#dog2").hide();
+	$("#dog3").hide();
+	$("#dog2").fadeIn(10000);
+	$("#dog1").fadeIn(20000);
+	$("#dog3").fadeIn(30000);
+
+
+
+});
---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$(".name").hide();
+	$(".name").fadeIn(1000);
+	
+	$("a[href^= .about").on("click")
+
+});
