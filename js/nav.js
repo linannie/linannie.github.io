486fb1b8ee98620042d335dda8a6895d60906152
Annie  Lin <a.popcorn2@gmail.com>
2021-09-07 15:55:41 -0700

    revamping header and other styles

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+document.write('<div id="top-bar">\
+			<div id="nameDiv">\
+			<a href="index.html" style= "text-decoration:none; font-family: "><h1 class="name">Annie Lin</h1></a>\
+			</div>\
+			<div id="links">\
+			<p style="text-align:right;">\
+				<a class="link" rel="Projects" href="index.html#allProj" style= "text-decoration:none;">Work</a>\
+				&nbsp;&nbsp;&nbsp;\
+				<a class="link" rel= "about me!" href= "about.html" style= "text-decoration:none;"> About</a>&nbsp;&nbsp;&nbsp;&nbsp;\
+				<a class="link" rel= "Resume" href= "files/CV5.0.pdf" style= "text-decoration:none;"> Resume</a></p>\
+			</div>\
+		</div>\
+');
