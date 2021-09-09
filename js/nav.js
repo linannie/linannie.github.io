8af036baa05cd69e089610b4ffdccfbe71fa6736
Annie  Lin <a.popcorn2@gmail.com>
2021-09-08 20:07:32 -0700

    open resume on new page

--- js/nav.js
+++ js/nav.js
@@ -7,7 +7,7 @@ document.write('<div id="top-bar">\
 				<a class="link" rel="Projects" href="index.html#allProj" style= "text-decoration:none;">Work</a>\
 				&nbsp;&nbsp;&nbsp;\
 				<a class="link" rel= "about me!" href= "about.html" style= "text-decoration:none;"> About</a>&nbsp;&nbsp;&nbsp;&nbsp;\
-				<a class="link" rel= "Resume" href= "files/uxRes2021.pdf" style= "text-decoration:none;"> Resume</a></p>\
+				<a class="link" target="_blank" rel= "Resume" href= "files/uxRes2021.pdf" style= "text-decoration:none;"> Resume</a></p>\
 			</div>\
 		</div>\
 ');
