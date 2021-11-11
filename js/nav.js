6cbe569882fbe7615d076a6d9a3980fe0008cfb5
Annie  Lin <a.popcorn2@gmail.com>
2021-11-11 12:05:26 -0800

    i made so many changes, aaa redesign

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,13 @@
 document.write('<div id="top-bar">\
 			<div id="nameDiv">\
-			<a href="index.html" style= "text-decoration:none; font-family: "><h1 class="name">Annie Lin</h1></a>\
+			<a href="index.html" style= "text-decoration:none; font-family: "><h1 class="name">al</h1></a>\
 			</div>\
 			<div id="links">\
 			<p style="text-align:right;">\
-				<a class="link" rel="Projects" href="index.html#allProj" style= "text-decoration:none;">Work</a>\
+				<a class="link" rel="Projects" href="index.html#allProj" style= "text-decoration:none;">work</a>\
 				&nbsp;&nbsp;&nbsp;\
-				<a class="link" rel= "about me!" href= "about.html" style= "text-decoration:none;"> About</a>&nbsp;&nbsp;&nbsp;&nbsp;\
-				<a class="link" target="_blank" rel= "Resume" href= "files/uxRes2021.pdf" style= "text-decoration:none;"> Resume</a></p>\
+				<a class="link" rel= "about me!" href= "about.html" style= "text-decoration:none;"> about</a>&nbsp;&nbsp;&nbsp;&nbsp;\
+				<a class="link" target="_blank" rel= "Resume" href= "files/uxRes2021.pdf" style= "text-decoration:none;"> resume</a></p>\
 			</div>\
 		</div>\
 ');
